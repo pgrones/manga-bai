dfe9c4396bb733d346a3bc3930dc1bd6abca6e63
pgr3931 <45991112+pgr3931@users.noreply.github.com>
2022-05-19 17:07:34 +0200

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    'react': {
+      version: 'detect'
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'prettier' // Make sure this is always the last element in the array.
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'eqeqeq': 'error',
+    'prefer-arrow-callback': 'error',
+    'prefer-const': [
+      'error',
+      {
+        ignoreReadBeforeAssign: true
+      }
+    ],
+    'semi': [2, 'always'],
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/self-closing-comp': 'error',
+    'react/display-name': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/explicit-function-return-type': 'off'
+  }
+};
